d6555ebfe5ed644c8870a7975f8846965d68191e
Jake Pearson <notjakepearson@gmail.com>
2018-01-04 16:17:19 -0700

    Added HashAndEncode function

---
+++ password/password.go
@@ -0,0 +1,13 @@
+package password
+
+import (
+	"crypto/sha512"
+	"encoding/base64"
+	"io"
+)
+
+func HashAndEncode(input string) string {
+	sha := sha512.New()
+	io.WriteString(sha, input)
+	return base64.StdEncoding.EncodeToString(sha.Sum(nil))
+}
